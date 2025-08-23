1a080b83e1574c912af15a15ddfa427ccf692f6b
Herjuna <76900479+Herjuna3008@users.noreply.github.com>
2025-08-24 03:52:10 +0700

    Update and rename exportUtils.ts to exportUtils.js
    change exportUtils to js

---
+++ backend/src/utils/exportUtils.js
@@ -0,0 +1,10 @@
+import pdf from "html-pdf"
+
+export function exportToPDF(html, options = {}) {
+  return new Promise((resolve, reject) => {
+    pdf.create(html, { format: "A4", ...options }).toBuffer((err, buffer) => {
+      if (err) return reject(err)
+      resolve(buffer)
+    })
+  })
+}
