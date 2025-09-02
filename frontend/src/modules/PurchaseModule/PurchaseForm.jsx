8548e7ba81a9707947cce8f75c9a5e65aae9ffb0
Herjuna <76900479+Herjuna3008@users.noreply.github.com>
2025-09-03 02:18:00 +0700

    feat: add purchase form table and stock refresh

--- frontend/src/modules/PurchaseModule/PurchaseForm.jsx
+++ frontend/src/modules/PurchaseModule/PurchaseForm.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Form, Input, InputNumber, Button, Divider, DatePicker } from 'antd';
+import { Row, Col, Form, Input, InputNumber, Button, Divider, DatePicker, Table } from 'antd';
 import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
 import dayjs from 'dayjs';
 import SelectAsync from '@/components/SelectAsync';
@@ -33,66 +33,77 @@ export default function PurchaseForm() {
         </Col>
       </Row>
       <Divider dashed />
-      <Row gutter={[12, 0]}>
-        <Col span={8}>
-          <p>Product</p>
-        </Col>
-        <Col span={4}>
-          <p>Quantity</p>
-        </Col>
-        <Col span={4}>
-          <p>Cost</p>
-        </Col>
-        <Col span={4}>
-          <p>Total</p>
-        </Col>
-      </Row>
       <Form.List name="items">
-        {(fields, { add, remove }) => (
-          <>
-            {fields.map((field) => (
-              <Row key={field.key} gutter={[12, 12]} style={{ position: 'relative' }}>
-                <Col span={8}>
-                  <Form.Item
-                    name={[field.name, 'product']}
-                    rules={[{ required: true, message: 'Product is required' }]}
-                  >
-                    <SelectAsync entity={'products'} outputValue={'id'} displayLabels={['name']} />
-                  </Form.Item>
-                </Col>
-                <Col span={4}>
-                  <Form.Item
-                    name={[field.name, 'quantity']}
-                    rules={[{ required: true, message: 'Quantity is required' }]}
-                  >
-                    <InputNumber min={0} style={{ width: '100%' }} />
-                  </Form.Item>
-                </Col>
-                <Col span={4}>
-                  <Form.Item
-                    name={[field.name, 'cost']}
-                    rules={[{ required: true, message: 'Cost is required' }]}
-                  >
-                    <InputNumber min={0} style={{ width: '100%' }} />
-                  </Form.Item>
-                </Col>
-                <Col span={4}>
-                  <Form.Item name={[field.name, 'total']}>
-                    <InputNumber readOnly min={0} style={{ width: '100%' }} />
-                  </Form.Item>
-                </Col>
-                <div style={{ position: 'absolute', right: '-20px', top: '5px' }}>
-                  <DeleteOutlined onClick={() => remove(field.name)} />
-                </div>
-              </Row>
-            ))}
-            <Form.Item>
-              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                Add Item
-              </Button>
-            </Form.Item>
-          </>
-        )}
+        {(fields, { add, remove }) => {
+          const columns = [
+            {
+              title: 'Product',
+              dataIndex: 'product',
+              render: (_, record) => (
+                <Form.Item
+                  name={[record.name, 'product']}
+                  rules={[{ required: true, message: 'Product is required' }]}
+                  style={{ marginBottom: 0 }}
+                >
+                  <SelectAsync entity={'products'} outputValue={'id'} displayLabels={['name']} />
+                </Form.Item>
+              ),
+            },
+            {
+              title: 'Quantity',
+              dataIndex: 'quantity',
+              render: (_, record) => (
+                <Form.Item
+                  name={[record.name, 'quantity']}
+                  rules={[{ required: true, message: 'Quantity is required' }]}
+                  style={{ marginBottom: 0 }}
+                >
+                  <InputNumber min={0} style={{ width: '100%' }} />
+                </Form.Item>
+              ),
+            },
+            {
+              title: 'Cost',
+              dataIndex: 'cost',
+              render: (_, record) => (
+                <Form.Item
+                  name={[record.name, 'cost']}
+                  rules={[{ required: true, message: 'Cost is required' }]}
+                  style={{ marginBottom: 0 }}
+                >
+                  <InputNumber min={0} style={{ width: '100%' }} />
+                </Form.Item>
+              ),
+            },
+            {
+              title: 'Total',
+              dataIndex: 'total',
+              render: (_, record) => (
+                <Form.Item name={[record.name, 'total']} style={{ marginBottom: 0 }}>
+                  <InputNumber readOnly min={0} style={{ width: '100%' }} />
+                </Form.Item>
+              ),
+            },
+            {
+              title: '',
+              dataIndex: 'actions',
+              render: (_, record) => (
+                <DeleteOutlined onClick={() => remove(record.name)} />
+              ),
+            },
+          ];
+
+          return (
+            <>
+              <Table pagination={false} dataSource={fields} columns={columns} rowKey="key" />
+              <Form.Item>
+                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                  Add Item
+                </Button>
+              </Form.Item>
+            </>
+          );
+        }}
       </Form.List>
     </>
   );
